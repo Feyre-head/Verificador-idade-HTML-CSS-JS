a855b63294f84fa989f427ec75352aaf44d51fcf
Feyre <inaraoenning@gmail.com>
2024-11-14 09:54:15 -0500

    refactor

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ function verificar() {
   if (anoFormulario == 0 || anoFormulario > anoAtual) {
     alert("[ERRO] Verifique os dados e tente novamente!");
   } else {
-    var sexoFormulario = document.getElementsByName("radsex");
-    var idade = anoAtual - anoFormulario;
-    var gênero = "";
-    var img = document.createElement("img"); //Cria uma tag img
+    let sexoFormulario = document.getElementsByName("radsex");
+    let idade = anoAtual - anoFormulario;
+    let gênero = "";
+    let img = document.createElement("img");
     img.width = 100;
-    img.setAttribute("id", "foto"); //Criei um id foto
+    img.setAttribute("id", "foto");
 
     if (sexoFormulario[0].checked) {
       gênero = "Homem";
       if (idade >= 0 && idade < 13) {
-        //CriançaM
+        //Criança
         img.setAttribute("src", "./imagens/criancaM.png");
       } else if (idade < 18) {
-        //AdolescenteM
+        //Adolescente
         img.setAttribute("src", "./imagens/AdolescenteM.png");
       } else if (idade < 54) {
         //Adulto
@@ -30,21 +30,22 @@ function verificar() {
       }
     } else if (sexoFormulario[1].checked) {
       gênero = "Mulher";
+
       if (idade >= 0 && idade < 13) {
-        //CriançaF
-        img.setAttribute("src", "/imagens/criancaF.png");
+        //Criança
+        img.setAttribute("src", "./imagens/criancaF.png");
       } else if (idade < 18) {
         //Adolescente
         img.setAttribute("src", "./imagens/AdolescenteF.png");
       } else if (idade < 54) {
         //Adulto
-        img.setAttribute("src", "/imagens/AdultoF.png");
+        img.setAttribute("src", "./imagens/AdultoF.png");
       } else {
         //Idosa
         img.setAttribute("src", "./imagens/Idosa.png");
       }
     }
+    res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`;
+    res.appendChild(img);
   }
-  res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`;
-  res.appendChild(img);
 }
